c838c4c17888a83287b480f9c408abfcdf796d2c
Arjan van de Ven <arjan@linux.intel.com>
2010-08-01 10:29:43 -0400

    start getting msr stuff in

--- intel_cpus.cpp
+++ intel_cpus.cpp
@@ -1,27 +1,101 @@
 #include "cpu.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+
+static uint64_t get_msr(int cpu, uint64_t offset)
+{
+	ssize_t retval;
+	uint64_t msr;
+	int fd;
+	char msr_path[256];
+
+	fd  =sprintf(msr_path, "/dev/cpu/%d/msr", cpu);
+	fd = open(msr_path, O_RDONLY);
+
+	retval = pread(fd, &msr, sizeof msr, offset);
+	if (retval != sizeof msr) {
+		fprintf(stderr, "pread cpu%d 0x%llx \n", cpu, offset);
+		_exit(-2);
+	}
+	close(fd);
+	return msr;
+}
 
 void nhm_core::measurement_start(void)
 {
+	abstract_cpu::measurement_start();
+
+	aperf_before = get_msr(number, MSR_APERF);
+	mperf_before = get_msr(number, MSR_MPERF);
+	c3_before    = get_msr(number, MSR_CORE_C3_RESIDENCY);
+	c6_before    = get_msr(number, MSR_CORE_C6_RESIDENCY);
 }
 
 void nhm_core::measurement_end(void)
 {
-}
+	unsigned int i;
+	for (i = 0; i < children.size(); i++)
+		if (children[i])
+			children[i]->measurement_end();
 
-void nhm_core::consolidate_children(void)
-{
+
+	aperf_after = get_msr(number, MSR_APERF);
+	mperf_after = get_msr(number, MSR_MPERF);
+	c3_after    = get_msr(number, MSR_CORE_C3_RESIDENCY);
+	c6_after    = get_msr(number, MSR_CORE_C6_RESIDENCY);
+
+
+
+
+	for (i = 0; i < states.size(); i++) {
+		struct power_state *state = states[i];
+
+		if (state->after_count == 0) {
+			cout << "after count is 0\n";
+			continue;
+		}
+
+		if (state->after_count != state->before_count) {
+			cout << "count mismatch\n";
+			continue;
+		}
+
+		state->usage_delta =    (state->usage_after    - state->usage_before)    / state->after_count;
+		state->duration_delta = (state->duration_after - state->duration_before) / state->after_count;
+	}
 }
 
 void nhm_package::measurement_start(void)
 {
+	abstract_cpu::measurement_start();
 }
 
 void nhm_package::measurement_end(void)
 {
-}
+	unsigned int i;
+	for (i = 0; i < children.size(); i++)
+		if (children[i])
+			children[i]->measurement_end();
 
-void nhm_package::consolidate_children(void)
-{
+	for (i = 0; i < states.size(); i++) {
+		struct power_state *state = states[i];
+
+		if (state->after_count == 0) {
+			cout << "after count is 0\n";
+			continue;
+		}
+
+		if (state->after_count != state->before_count) {
+			cout << "count mismatch\n";
+			continue;
+		}
+
+		state->usage_delta =    (state->usage_after    - state->usage_before)    / state->after_count;
+		state->duration_delta = (state->duration_after - state->duration_before) / state->after_count;
+	}
 }
 
--- intel_cpus.h
+++ intel_cpus.h
@@ -1,3 +1,16 @@
+#include <stdint.h>
+
+#define MSR_TSC				0x10
+#define MSR_NEHALEM_PLATFORM_INFO	0xCE
+#define MSR_NEHALEM_TURBO_RATIO_LIMIT	0x1AD
+#define MSR_APERF			0xE8
+#define MSR_MPERF			0xE7
+#define MSR_PKG_C3_RESIDENCY		0x3F8
+#define MSR_PKG_C6_RESIDENCY		0x3F9
+#define MSR_CORE_C3_RESIDENCY		0x3FC
+#define MSR_CORE_C6_RESIDENCY		0x3FD
+
+
 class nhm_package: public cpu_package 
 {
 public:
@@ -9,6 +22,11 @@ public:
 
 class nhm_core: public cpu_core 
 {
+private:
+	uint64_t	aperf_before, mperf_before;
+	uint64_t	aperf_after, mperf_after;
+	uint64_t	c3_before, c3_after;
+	uint64_t	c6_before, c6_after;
 public:
 	virtual void 	measurement_start(void);
 	virtual void 	measurement_end(void);

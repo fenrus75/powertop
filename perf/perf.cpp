443f617e1162b51f248a72b803d2cf351c228797
Arjan van de Ven <arjan@linux.intel.com>
2011-01-02 10:54:22 -0800

    more translation bugs

--- perf/perf.cpp
+++ perf/perf.cpp
@@ -108,7 +108,7 @@ void perf_event::create_perf_event(char *eventname, int _cpu)
 
 	if (perf_fd < 0) {
 		reset_display();
-		fprintf(stderr, _("PowerTOP " POWERTOP_VERSION " needs the kernel to support the 'perf' subsystem\n"));
+		fprintf(stderr, _("PowerTOP %s needs the kernel to support the 'perf' subsystem\n"), POWERTOP_VERSION);
 		fprintf(stderr, _("as well as support for trace points in the kernel:\n"));
 		fprintf(stderr, _("CONFIG_PERF_EVENTS=y\nCONFIG_PERF_COUNTERS=y\nCONFIG_TRACEPOINTS=y\nCONFIG_TRACING=y\n"));
 		exit(EXIT_FAILURE);

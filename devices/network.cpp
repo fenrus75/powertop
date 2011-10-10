d5cc62f70053c122ef623b4d6575e2bf853855c8
Arjan van de Ven <arjan@linux.intel.com>
2011-10-09 21:46:26 -0400

    fix up more wreckage after merging the android patches

--- devices/network.cpp
+++ devices/network.cpp
@@ -54,6 +54,8 @@ extern "C" {
 
 static map<string, class network *> nics;
 
+#ifdef DISABLE_TRYCATCH
+
 static inline void ethtool_cmd_speed_set(struct ethtool_cmd *ep,
 						__u32 speed)
 {
@@ -66,6 +68,8 @@ static inline __u32 ethtool_cmd_speed(struct ethtool_cmd *ep)
 {
 	return (ep->speed_hi << 16) | ep->speed;
 }
+
+#endif
 
 static void do_proc_net_dev(void)
 {

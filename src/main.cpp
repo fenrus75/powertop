b8af9381618d0ce5508685f75391cf0b6172a946
Joerg Mayer <jmpt@loplof.de>
2012-08-23 23:29:49 +0200

    Fix spelling of command line argument --quite to --quiet. Add a manpage entry for --quiet, which was missing.
    Signed-off-by: Joerg Mayer <jmpt@loplof.de>

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ static const struct option long_options[] =
 	{"time", optional_argument, NULL, 't'},
 	{"iteration", optional_argument, NULL, 'i'},
 	{"workload", optional_argument, NULL, 'w'},
-	{"quite", optional_argument, NULL, 'q'},
+	{"quiet", optional_argument, NULL, 'q'},
 	{NULL, 0, NULL, 0}
 };
 
@@ -112,7 +112,7 @@ static void print_usage()
 	printf("--time%s \t %s\n",_("[=seconds]"), _("generate a report for 'x' seconds"));
 	printf("--iteration%s\n", _("[=iterations] number of times to run each test"));
 	printf("--workload%s \t %s\n", _("[=workload]"), _("file to execute for workload"));
-	printf("--quite \t %s\n", _("supress stderr output"));
+	printf("--quiet \t\t %s\n", _("supress stderr output"));
 	printf("--help \t\t\t %s\n",_("print this help menu"));
 	printf("\n");
 	printf("%s\n\n",_("For more help please refer to the README"));

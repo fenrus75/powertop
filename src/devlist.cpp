b3c44a8406e4b4e82ad06bf0b54c7411a66735af
Namhyung Kim <namhyung@gmail.com>
2012-09-19 00:13:17 +0900

    devlist: Remove an unnecessary sprintf
    The 'filename' is not used in the loop anymore.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- src/devlist.cpp
+++ src/devlist.cpp
@@ -147,7 +147,6 @@ void collect_open_devices(void)
 					continue;
 				dev->pid = strtoull(entry->d_name, NULL, 10);
 				strncpy(dev->device, link, 251);
-				sprintf(filename, "/proc/%s/comm", entry->d_name);
 				strncpy(dev->comm, read_sysfs_string("/proc/%s/comm", entry->d_name).c_str(), 31);
 				target->push_back(dev);
 

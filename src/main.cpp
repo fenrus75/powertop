1333223c1ed0773a31a533ed0ec794d15b7b10d3
Alexandra Yates <alexandra.yates@linux.intel.com>
2014-05-12 16:12:06 -0700

    Revert "Adding a clean_shutdown function"
    This reverts commit 5f5976f8b42e4e4717f2d573cf43cb87ccd88fa2.

--- src/main.cpp
+++ src/main.cpp
@@ -359,17 +359,6 @@ static void powertop_init(void)
 	initialized = 1;
 }
 
-void clean_shutdown()
-{
-	close_results();
-	close_display();
-	clean_open_devices();
-	clear_all_devices();
-	clear_all_cpus();
-
-	return;
-}
-
 
 int main(int argc, char **argv)
 {
@@ -497,7 +486,9 @@ int main(int argc, char **argv)
 	clear_tuning();
 	reset_display();
 
-	clean_shutdown();
+	clean_open_devices();
+	clear_all_devices();
+	clear_all_cpus();
 
 	return 0;
 }

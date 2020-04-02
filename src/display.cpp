fenrus75/powertop

commit 152a39949691d34e1c6d67272f3b63cbbe61bf81

Revert "configure.ac: version strings from git describe"
This doesn't work as well as I thought it would. So, let's remove it. As
a result of the revert, remove a reference to a now-dead preprocessor
define (PACKAGE_SHORT_VERSION).

This reverts commit 2daaaf54d07340610af759cc7f4c41742abb3277.

Signed-off-by: Joe Konno <joe.konno@intel.com>

configure.ac
@@ -2,8 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ([2.68])
-AC_INIT([powertop], m4_esyscmd_s([cat version-long]),
-	[powertop@lists.01.org], [], [https://01.org/powertop])
+AC_INIT([powertop], [2.9-pre], [powertop@lists.01.org], [], [https://01.org/powertop])
 AM_INIT_AUTOMAKE([
 	-Wall
 	1.12.2
@@ -152,8 +151,6 @@ AC_SEARCH_LIBS([inet_aton], [resolv], [], [
 	AC_MSG_ERROR([libresolv is required but was not found])
 ], [])
 
-AC_DEFINE([PACKAGE_SHORT_VERSION], m4_esyscmd_s([cat version-short]),
-	[Short package version])
 
 AS_IF([`pkg-config --exists bash-completion`], [
 	bashcompletiondir=`pkg-config --variable=completionsdir --define-variable=prefix=${prefix} bash-completion`

.gitignore
@@ -89,5 +89,4 @@ Makefile.in
 /src/powertop
 /stamp-h1
 /stamp-po
-/version*
 tags

Makefile.am
@@ -15,6 +15,4 @@ EXTRA_DIST = \
 	TODO \
 	Android.mk \
 	COPYING \
-	version-long \
-	version-short \
 	autogen.sh

autogen.sh
@@ -1,4 +1,3 @@
 #!/bin/sh	
 
-sh scripts/version
 autoreconf --install --verbose

scripts/version
@@ -1,13 +1,0 @@
-#!/bin/sh
-
-git branch > /dev/null 2>&1
-if [ "$?" = "0" ]; then
-	LONG=$(git describe --abbrev=7 --tags --always --dirty 2> /dev/null)
-	SHORT=\"$(git describe --tags --abbrev=0 2> /dev/null)\"
-else
-	LONG="RUN-VERSION-SCRIPT-IN-GIT-REPOSITORY-ONLY"
-	SHORT=\"$LONG\"
-fi
-
-echo $LONG  > version-long
-echo $SHORT > version-short

commit a5a5066fdb3c8dd111703402e032f94297294f4d

src/display.cpp: Fix: Add the missing part of the patch changes
This patch fixes the part of missing patch changes and updates the
version in configure.ac to 2.12-pre

fixes: 152a39949691 ("Revert "configure.ac: version strings from git
describe"")

Reported-by: Anthony Wong <yp@anthonywong.net>
Signed-off-by: Joe Konno <joe.konno@intel.com>
Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>

configure.ac
@@ -2,7 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ([2.69])
-AC_INIT([powertop], [2.9-pre], [powertop@lists.01.org], [], [https://01.org/powertop])
+AC_INIT([powertop], [2.12-pre], [powertop@lists.01.org], [], [https://01.org/powertop])
 AM_INIT_AUTOMAKE([
 	-Wall
 	1.12.2

src/display.cpp
@@ -117,7 +117,7 @@ void show_tab(unsigned int tab)
 
 	wattrset(tab_bar, A_REVERSE);
 	mvwprintw(tab_bar, 0,0, "%120s", "");
-	mvwprintw(tab_bar, 0,0, "PowerTOP %s", PACKAGE_SHORT_VERSION);
+	mvwprintw(tab_bar, 0,0, "PowerTOP %s", PACKAGE_VERSION);
 
 	bottom_line = newwin(1, 0, LINES-1, 0);
 	wattrset(bottom_line, A_REVERSE);
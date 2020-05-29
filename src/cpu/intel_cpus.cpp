fenrus75/powertop

commit b637c171c36209134b381e8d88c28929f2229c25

intel_cpus: Enable Sky Lake server support in PowerTop
Add PowerTop support for Sky Lake server.

Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>

src/cpu/intel_cpus.cpp
@@ -65,6 +65,7 @@ static int intel_cpu_models[] = {
 	0x4D,	/* AVN */
 	0x4F,	/* BDX */
 	0x4E,	/* SKY */
+	0x55,	/* SKY-X */
 	0x56,	/* BDX-DE */
 	0x5C,	/* BXT-P */
 	0x5E,	/* SKY */
@@ -176,6 +177,7 @@ nhm_core::nhm_core(int model)
 		case 0x3E:      /* IVB Xeon */
 		case 0x45:	/* HSW-ULT */
 		case 0x4E:	/* SKY */
+		case 0x55:	/* SKY-X */
 		case 0x5E:	/* SKY */
 		case 0x5C:      /* BXT-P */
 		case 0x66:	/* CNL-U/Y */
@@ -367,6 +369,7 @@ nhm_package::nhm_package(int model)
 		case 0x3E:      /* IVB Xeon */
 		case 0x45:	/* HSW-ULT */
 		case 0x4E:	/* SKY */
+		case 0x55:	/* SKY-X */
 		case 0x5C:	/* BXT-P */
 		case 0x5E:	/* SKY */
 		case 0x66:	/* CNL-U/Y */
@@ -410,6 +413,7 @@ nhm_package::nhm_package(int model)
 		case 0x3D:	/* BDW */
 		case 0x45:	/* HSW */
 		case 0x4E:	/* SKY */
+		case 0x55:	/* SKY-X */
 		case 0x5C:	/* BXT-P */
 		case 0x5E:	/* SKY */
 		case 0x66:	/* CNL-U/Y */

commit 674d8f5638fcc992d164145427d9551d49acc29e

intel_cpus: Fix SKY-X definition
Much like #51, SKY-X was incorrectly defined, so let's fix it.

Fixes: b637c171c362 ("intel_cpus: Enable Sky Lake server support in PowerTop")
Signed-off-by: Joe Konno <joe.konno@intel.com>

src/cpu/intel_cpus.cpp
@@ -417,7 +417,6 @@ nhm_package::nhm_package(int model)
 		case 0x3D:	/* BDW */
 		case 0x45:	/* HSW */
 		case 0x4E:	/* SKY */
-		case 0x55:	/* SKY-X */
 		case 0x5C:	/* BXT-P */
 		case 0x5E:	/* SKY */
 		case 0x5F:	/* DNV */
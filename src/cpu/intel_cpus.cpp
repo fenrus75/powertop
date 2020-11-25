b4026d1996a6e091aef313c0b2ad4851b1dee56f
Gayatri Kammela <gayatri.kammela@intel.com>
2020-11-24 17:41:40 -0800

    intel_cpus: Enable Alder Lake mobile platform support in PowerTop
    Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>

--- src/cpu/intel_cpus.cpp
+++ src/cpu/intel_cpus.cpp
@@ -81,6 +81,7 @@ static int intel_cpu_models[] = {
 	0X8F, 	/* SAPPHIRERAPIDS_X */
 	0x96,	/* EHL */
 	0x97,	/* ADL_DESKTOP */
+	0x9A,	/* ADL_MOBILE */
 	0x9C,	/* JSL */
 	0x9E,	/* KBL */
 	0xA5,   /* CML_DESKTOP */
@@ -197,6 +198,7 @@ nhm_core::nhm_core(int model)
 		case 0x8F:	/* SAPPHIRERAPIDS_X */
 		case 0x96:	/* EHL */
 		case 0x97:	/* ADL_DESKTOP */
+		case 0x9A:	/* ADL_MOBILE */
 		case 0x9C:	/* JSL */
 		case 0x9E:	/* KBL */
 		case 0xA5:      /* CML_DESKTOP */
@@ -389,6 +391,7 @@ nhm_package::nhm_package(int model)
 		case 0x8F:	/* SAPPHIRERAPIDS_X */
 		case 0x96:      /* EHL */
 		case 0x97:	/* ADL_DESKTOP */
+		case 0X9A:	/* ADL_MOBILE */
 		case 0x9C:	/* JSL */
 		case 0x9E:	/* KBL */
 		case 0xA5:      /* CML_DESKTOP */
@@ -434,6 +437,7 @@ nhm_package::nhm_package(int model)
 		case 0x8E:	/* KBL */
 		case 0x96:      /* EHL */
 		case 0x97:	/* ADL_DESKTOP */
+		case 0x9A:	/* ADL_MOBILE */
 		case 0x9C:	/* JSL */
 		case 0x9E:	/* KBL */
 		case 0xA5:      /* CML_DESKTOP */

ff2c771ad50f6943c796c431de94107df6242ff6
Gayatri Kammela <gayatri.kammela@linux.intel.com>
2022-03-18 12:48:10 -0700

    intel_cpus: Enable Raptor Lake platform support in PowerTop
    Signed-off-by: Gayatri Kammela <gayatri.kammela@linux.intel.com>

--- src/cpu/intel_cpus.cpp
+++ src/cpu/intel_cpus.cpp
@@ -89,6 +89,7 @@ static int intel_cpu_models[] = {
 	0xA5,   /* CML_DESKTOP */
 	0xA6,   /* CML_MOBILE */
 	0xA7,	/* RKL_DESKTOP */
+	0xB7,	/* RPL_DESKTOP */
 	0xBE,	/* ADL_N */
 	0	/* last entry must be zero */
 };
@@ -209,6 +210,7 @@ nhm_core::nhm_core(int model)
 		case 0xA5:      /* CML_DESKTOP */
 		case 0xA6:      /* CML_MOBILE */
 		case 0xA7:	/* RKL_DESKTOP */
+		case 0xB7:	/* RPL_DESKTOP */
 		case 0xBE:	/* ADL_N */
 			has_c7_res = 1;
 	}
@@ -405,6 +407,7 @@ nhm_package::nhm_package(int model)
 		case 0xA5:      /* CML_DESKTOP */
 		case 0xA6:      /* CML_MOBILE */
 		case 0xA7:	/* RKL_DESKTOP */
+		case 0xB7:	/* RPL_DESKTOP */
 		case 0xBE:	/* ADL_N */
 			has_c2c6_res=1;
 			has_c7_res = 1;
@@ -454,6 +457,7 @@ nhm_package::nhm_package(int model)
 		case 0xA5:      /* CML_DESKTOP */
 		case 0xA6:      /* CML_MOBILE */
 		case 0xA7:	/* RKL_DESKTOP */
+		case 0xB7:	/* RPL_DESKTOP */
 		case 0xBE:	/* ADL_N */
 			has_c8c9c10_res = 1;
 			break;
